3beceb4d57dff0aa12c088345925abfafc540b8d
chunderfam-stack <chunderfam@gmail.com>
2026-02-06 11:39:46 -0700

    login page done!

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+    <main>
+        <div class="bg-yellow">
+            <h1>Login to save your scores!</h1>
+            <form method="get" action="game">
+                <div>
+                    <input type="text" placeholder="your@email.com" />
+                </div>
+                <div>
+                    <input type="password" placeholder="password" />
+                </div>
+                <button type="submit">Login</button>
+                <button type="submit">Create</button>
+            </form>
+        </div>
     </main>
   );
 }
