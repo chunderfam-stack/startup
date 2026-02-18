76a80e0823c969143eb40649c5569c55565daacc
chunderfam-stack <chunderfam@gmail.com>
2026-02-17 23:40:53 -0700

    package changes?

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -1,3 +1,22 @@
-export function Authenticated({userName, logOut}){
+import React from 'react';
 
+import { useNavigate} from 'react-router-dom';
+
+import Button from 'react-bootstrap/Button';
+
+export function Authenticated(props){
+    const navigate = useNavigate();
+
+    function logout(){
+        localStorage.removeItem('userName')
+        props.logOut()
+    }
+
+    return(
+        <div>
+            <div className='playerName'>{props.userName}</div>
+            <Button onClick={() => navigate('/game')}>Play</Button>
+            <Button onClick={() => logout()}>Logout</Button>
+        </div>
+    )
 }
