d742ece6cd8be63eeedef4cfbe2ce3f3dba254b1
chunderfam-stack <chunderfam@gmail.com>
2026-02-24 07:54:22 -0700

    functioning button?

--- src/game/playArea.jsx
+++ src/game/playArea.jsx
@@ -4,28 +4,50 @@ import './game.css';
 
 export function PlayArea(){
     const [timer, setTimer] = React.useState(0);
+    const [inPlay, setPlay] = React.useState(false);
+    const [startGame, setGame] = React.useState(false);
+    const [duckMode, setDuckMode] = React.useState(false);
+    function startPlay(){
+        setPlay(true);
+    }
+    function readyGame(){
+        setTimeout(() => startPlay(), Math.random() * 3000 + 500);
+    }
+    React.useEffect(() => {
+        if(!inPlay){
+            return;
+        }
+        const start = performance.now();
+            const interval = setInterval(() =>{
+            const now = performance.now();
+            setTimer(((now - start)).toFixed(1))
+        }, 10);
+        return () => clearInterval(interval);
+        
+    }, [inPlay]);
+    function StartDuckMode(){
+        setDuckMode(!duckMode);
+    }
+
     return (
         <>
             <div className="play-area">
-                <img src="../duck-placeholder.jpg" height="100" width="100"/>
-                <button className="click-button">
-                    <svg height="100" width="100">
-                        <circle cx="50" cy="50" r="43" stroke="none" fill="Green"/>
-                        <circle cx="50" cy="50" r="49" fill="none" stroke="Green"/>
-                    </svg> 
-            </button>
-            <img src="../duck-placeholder.jpg" height="100" width="100"/>
+            {duckMode === true && <img src="../duck-placeholder.jpg" height="100" width="100"/>}
+                <button className="click-button" onClick={() => readyGame()}>
+                    {inPlay === false && (<p className='button-text'>Click to Play!</p>)}
+                </button>
+            {duckMode === true && <img src="../duck-placeholder.jpg" height="100" width="100"/>}
             </div>
 
             <div className="below-play">
                 <div className="form-check switch">
-                    <input class = "form-check-input" type="checkbox"/>
+                    <input className = "form-check-input" type="checkbox" onChange={StartDuckMode}/>
                     <label className="form-check-label">
                         <span className="slider round">Duck Mode</span>
                     </label>
                 </div>
-                <p id='timer'>0.567ms</p>
-            </div>
+                <p id='timer'>{timer}ms</p>
+            </div>  
         </>
         
     )
