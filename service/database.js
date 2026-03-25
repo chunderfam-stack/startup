aca3fd2fab7c734485f8d50ae2b525f50e0a35d9
chunderfam-stack <chunderfam@gmail.com>
2026-03-24 18:34:06 -0600

    Added a semicolon

--- service/database.js
+++ service/database.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 const config = require('./dbConfig.json');
 
-const url = `mongodb+srv://${config.username}:${config.password}@${config.hostname}`
+const url = `mongodb+srv://${config.username}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('siteData');
 const userCollection = db.collection('userCollection');
